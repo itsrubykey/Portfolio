92fdd7cbf6648d9bee1d52625997e37fdf6941ca
itsrubykey <107823472+itsrubykey@users.noreply.github.com>
2023-09-26 15:14:39 -0500

    Update streamlit_app.py
    syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 ------------------
 
 #streamlit.text(fruityvice_response.json()) #writes json response to screen
-fruit_choice= streamlit.text_input('What fruit would you like information about?', 'Kiwi)
-streamlit.write('The user entered', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response= requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
