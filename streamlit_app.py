e1b830d5b61a03bdc237b201f471a678954347b1
itsrubykey <107823472+itsrubykey@users.noreply.github.com>
2023-09-26 15:12:33 -0500

    Update streamlit_app.py
    edit code to choose own fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,12 +23,14 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+------------------
+
+#streamlit.text(fruityvice_response.json()) #writes json response to screen
+fruit_choice= streamlit.text_input('What fruit would you like information about?', 'Kiwi)
+streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response= requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
-#streamlit.text(fruityvice_response.json()) #writes json response to screen
-
-
+fruityvice_response= requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 fruityvice_normalized= pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
