3b7bd11dd0953aa63a2c72538c91f13b4a7fded9
itsrubykey <107823472+itsrubykey@users.noreply.github.com>
2023-09-28 05:31:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,10 +64,10 @@ if streamlit.button('Get Fruit Load List'):
 
 
 
-streamlit.header("")
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write("Thank you for adding "+add_my_fruit+".")
+# streamlit.header("")
+# add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+# streamlit.write("Thank you for adding "+add_my_fruit+".")
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
