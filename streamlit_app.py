bcb91885b9c0c081ea6c9f468c2d517c58ed09b2
itsrubykey <107823472+itsrubykey@users.noreply.github.com>
2023-09-28 05:04:07 -0500

    Update streamlit_app.py
    tidy up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urlib.error import URLError
 
 streamlit.title('My Parents New Helthy Diner')
 
@@ -10,7 +14,6 @@ streamlit.text('🥑🍞 Avocade Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
    
-import pandas
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -28,7 +31,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response= requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 fruityvice_normalized= pandas.json_normalize(fruityvice_response.json())
@@ -38,7 +41,7 @@ streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 #connect to snowflake
-import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
