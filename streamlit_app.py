01462350f0772630be58b3b034c96551b2edb33d
itsrubykey <107823472+itsrubykey@users.noreply.github.com>
2023-09-27 20:40:02 -0500

    Update streamlit_app.py
    add second fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+streamlit.header("")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.text("Thank you for adding"+add_my_fruit)
+
+
